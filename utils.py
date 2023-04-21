bfb004d2db9021506f3a12667e39bb363a488a35
48801431 <127577524+48801431@users.noreply.github.com>
2023-04-21 12:51:52 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,1 +1,22 @@
-在上述代码中，我们创建了一个 ChatGPTClient 类，用于调用 ChatGPT 接口生成回复消息。在 __init__ 方法中，我们保存了 API 密钥和密钥，并指定了 ChatGPT 的 API 接口地址。在 generate_reply 方法中，我们使用 requests 库向 ChatGPT API 发送 POST 请求，并将请求参数和请求头进行了适当的设置。最后，我们从响应中提取回复消息，并返回给调用者。
+import requests
+
+class ChatGPTClient:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.endpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions'
+
+    def generate_reply(self, prompt):
+        headers = {'Authorization': f'Bearer {self.api_secret}'}
+        data = {
+            'prompt': prompt,
+            'max_tokens': 50,
+            'temperature': 0.7,
+            'stop': ['\n']
+        }
+        response = requests.post(self.endpoint, headers=headers, json=data)
+        if response.status_code == 200:
+            reply = response.json()['choices'][0]['text'].strip()
+            return reply
+        else:
+            return None
